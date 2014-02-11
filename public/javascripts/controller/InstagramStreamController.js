ecd332b5da142b4676d5f5b29e7ffd077361b8a1
arkadyp <arkady.pevzner@gmail.com>
2014-02-11 13:00:01 -0800

    Adds likedScore to instagrams

--- public/javascripts/controller/InstagramStreamController.js
+++ public/javascripts/controller/InstagramStreamController.js
@@ -6,12 +6,26 @@ function InstagramStreamController($scope, $http, $timeout) {
     $http.get('/instagrams/get.json').success(function(result) {
       var instagrams = result.data;
 
+      var instagramsByUser = {};
+      // var hashTags = {};
+
+      for(var i = 0; i < instagrams.length; i++) {
+        var username = instagrams[i].username;
+        var instagram = JSON.parse(instagrams[i].instagram);
+        if(!(username in instagramsByUser)) {
+          instagramsByUser[username] = {};
+          instagramsByUser[username].liked = 0;
+        }
+        instagramsByUser[username].liked += instagram.likes.count || 0;
+      }
+
       for(var i = 0; i < instagrams.length; i++) {
         var instagram = JSON.parse(instagrams[i].instagram);
         instagrams[i].instagram = instagram;
         instagrams[i].url = instagram.images.low_resolution.url;
         instagrams[i].text = (instagram.caption) ? instagram.caption.text : "";
         instagrams[i].created_at = new Date(instagrams[i].created_at);
+        instagrams[i].likedScore = (instagram.likes.count / instagramsByUser[instagrams[i].username].liked).toFixed(2);
       }
       
       $scope.instagrams = instagrams;
